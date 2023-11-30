ce134bc8484cb19acffe21e74758e47c5c2075d2
MEDFARDZ <146747219+MEDFARDZ@users.noreply.github.com>
2023-11-30 10:15:56 -0500

    Update bbx.py

--- bbx.py
+++ bbx.py
@@ -1,12 +1,13 @@
 import datetime
 
 
-
 class Personne:
     def __init__(self, nom, prenom, age):
         self.nom = nom
         self.prenom = prenom
         self.age = age
+
+
 class Adherent(Personne):
     def __init__(self, nom, prenom, age, numAdherent):
         super().__init__(nom, prenom, age)
@@ -14,9 +15,13 @@ class Adherent(Personne):
 
     def __eq__(self, other):
         return self.numAdherent == other.numAdherent
+
+
 class Livre:
-    def __init__(self, titre, emprunte=False):
+    def __init__(self, titre, nombrePages, ISBN, emprunte=False):
         self.titre = titre
+        self.nombrePages = nombrePages
+        self.ISBN = ISBN
         self.emprunte = emprunte
 
 class Emprunt:
@@ -48,11 +53,12 @@ class Bibliotheque:
         self.emprunts.append(emprunt)
 
     def retourner_emprunt(self, emprunt):
-        emprunt.date_retour = datetime.now().strftime("%Y-%m-%d")
+        emprunt.date_retour = datetime.datetime.now().strftime("%Y-%m-%d")
 
     def afficher_emprunts(self):
         for emprunt in self.emprunts:
             print(f"{emprunt.adherent.nom} {emprunt.adherent.prenom} a emprunté {emprunt.livre.titre} le {emprunt.date_emprunt}")
+
 
 class InterfaceUtilisateur:
     def __init__(self, bibliotheque):
@@ -84,7 +90,7 @@ class InterfaceUtilisateur:
                 prenom = input("Entrez le prénom de l'adhérent : ")
                 age = input("Entrez l'age de l'adhérent : ")
                 numAdherent = input("Entrez le ID de l'adhérent : ")
-                adherent = Adherent(nom, prenom, age,numAdherent)
+                adherent = Adherent(nom, prenom, age, numAdherent)
                 self.bibliotheque.ajouter_adherent(adherent)
             elif choix == '2':
                 numAdherent = input("Entrez le ID de l'adhérent à supprimer : ")
@@ -98,38 +104,61 @@ class InterfaceUtilisateur:
                     print(f"{adherent.numAdherent} {adherent.nom} {adherent.prenom} {adherent.age}")
             elif choix == '4':
                 titre = input("Entrez le titre du livre : ")
-                livre = Livre(titre)
+                nombrePages = input("Entrez le nombrePages du livre : ")
+                ISBN = input("Entrez le ISBN du livre : ")
+                livre = Livre(titre, nombrePages, ISBN)
                 self.bibliotheque.ajouter_livre(livre)
             elif choix == '5':
                 titre = input("Entrez le titre du livre à supprimer : ")
-                livre = Livre(titre)
-                self.bibliotheque.supprimer_livre(livre)
+                livre = next((l for l in self.bibliotheque.livres if l.titre == titre), None)
+                if livre:
+                    self.bibliotheque.supprimer_livre(livre)
+                else:
+                    print("Livre non trouvé.")
             elif choix == '6':
                 for livre in self.bibliotheque.livres:
-                    print(f"{livre.titre} est {'pas' if not livre.emprunte else ''} emprunté")
+                    print(f" Titre:{livre.titre} ISBN:{livre.ISBN}  est {'pas' if not livre.emprunte else ''} emprunté")
             elif choix == '7':
-                nom = input("Entrez le nom de l'adhérent qui emprunte : ")
-                prenom = input("Entrez le prénom de l'adhérent qui emprunte : ")
-                adherent = Adherent(nom, prenom)
+                numAdherent = input("Entrez le ID de l'adhérent qui emprunte : ")
+                adherent = next((a for a in self.bibliotheque.adherents if a.numAdherent == numAdherent), None)
+                if not adherent:
+                    print("Adhérent non trouvé.")
+                    continue
+
                 titre = input("Entrez le titre du livre emprunté : ")
-                livre = Livre(titre)
-                date_emprunt = datetime.now().strftime("%Y-%m-%d")
+                livre = next((l for l in self.bibliotheque.livres if l.titre == titre), None)
+                if not livre:
+                    print("Livre non trouvé.")
+                    continue
+
+                date_emprunt = datetime.datetime.now().strftime("%Y-%m-%d")
                 emprunt = Emprunt(adherent, livre, date_emprunt)
                 self.bibliotheque.ajouter_emprunt(emprunt)
-                self.bibliotheque.livres[self.bibliotheque.livres.index(livre)].emprunte = True
+                livre.emprunte = True
             elif choix == '8':
-                nom = input("Entrez le nom de l'adhérent qui retourne le livre : ")
-                prenom = input("Entrez le prénom de l'adhérent qui retourne le livre : ")
-                adherent = Adherent(nom, prenom)
+                numAdherent = input("Entrez le ID de l'adhérent qui retourne le livre : ")
+                adherent = next((a for a in self.bibliotheque.adherents if a.numAdherent == numAdherent), None)
+                if not adherent:
+                    print("Adhérent non trouvé.")
+                    continue
+
                 titre = input("Entrez le titre du livre à retourner : ")
-                livre = Livre(titre)
-                emprunt = Emprunt(adherent, livre, "inconnue")
-                self.bibliotheque.retourner_emprunt(emprunt)
-                self.bibliotheque.livres[self.bibliotheque.livres.index(livre)].emprunte = False
+                livre = next((l for l in self.bibliotheque.livres if l.titre == titre), None)
+                if not livre:
+                    print("Livre non trouvé.")
+                    continue
+
+                emprunt = next((e for e in self.bibliotheque.emprunts if e.adherent == adherent and e.livre == livre and e.date_retour is None), None)
+                if emprunt:
+                    self.bibliotheque.retourner_emprunt(emprunt)
+                    livre.emprunte = False
+                else:
+                    print("Emprunt non trouvé.")
             elif choix == '9':
                 self.bibliotheque.afficher_emprunts()
             else:
                 print("Choix invalide, veuillez réessayer.")
+
 
 bibliotheque = Bibliotheque()
 interface = InterfaceUtilisateur(bibliotheque)
